440d63c0a2b61ebe1e833466127c0db7cb5108d9
sinah müller-vietinghoff <sinah.mv@gmail.com>
2022-10-30 18:49:04 +0100

    Hallo

--- app/src/main/java/de/sinah/dopplereffelt/MainActivity.kt
+++ app/src/main/java/de/sinah/dopplereffelt/MainActivity.kt
@@ -2,10 +2,28 @@ package de.sinah.dopplereffelt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.bestaetigung).setOnClickListener {
+            berechnen(it) }
     }
+
+    private fun berechnen(view: View) {
+        val geschw = findViewById<EditText>(R.id.eingabeWert1)
+        val hertz = findViewById<EditText>(R.id.eingabeWert2)
+        //val schall = 343
+        val textView = findViewById<TextView>(R.id.textViewTest1)
+        val textView2 = findViewById<TextView>(R.id.textViewTest2)
+        textView.text = geschw.toString()
+        textView2.text = hertz.toString()
+    }
+
 }
